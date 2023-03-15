f47ea2e1bfe293371c49b3119f2d5346a3773b75
VaidyaHarshal <harshal.vaidya300@gmail.com>
2023-03-15 14:28:10 -0400

    Small changes

--- dayOne.py
+++ dayOne.py
@@ -1,8 +1,6 @@
 """ 
 Question 1:
-Write a program which will find all such numbers which are divisible by 7 
-but are not a multiple of 5, between 2000 and 3200 (both included).
-The numbers obtained should be printed in a comma-separated sequence on a single line.
+Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).The numbers obtained should be printed in a comma-separated sequence on a single line.
 """
 
 for i in range(2000,3201):         # For the range of i between the numbers 2000 and 3201
@@ -12,9 +10,7 @@ print("\b")
 
 """ 
 Question 2:
-Write a program which can compute the factorial of a given numbers.
-The results should be printed in a comma-separated sequence on a single line.
-Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence on a single line. Suppose the following input is supplied to the program: 8 Then, the output should be:40320
 """
 
 def find_factorial(num):
@@ -29,10 +25,7 @@ find_factorial(num)             # Calling the factorial function and passing the
 
 """ 
 Question 3:
-With a given integral number n, write a program to generate a dictionary 
-that contains (i, i x i) such that is an integral number between 1 and n (both included). 
-and then the program should print the dictionary.Suppose the following input is supplied to the program: 8
-Then, the output should be:
+With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.Suppose the following input is supplied to the program: 8 Then, the output should be:
 {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
 """
 
